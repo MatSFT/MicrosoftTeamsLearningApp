6ccfb89191f7d19b7578b39b2d1a8d84c39d6164
Matt Stankiewicz <mastank@microsoft.com>
2018-08-28 20:38:18 -0700

    Add GreetDialog

---
+++ Microsoft.Teams.Learning/Dialogs/GreetDialog.cs
@@ -0,0 +1,60 @@
+﻿//----------------------------------------------------------------------------------------------
+// <copyright file="GreetDialog.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+//----------------------------------------------------------------------------------------------
+
+namespace Microsoft.Teams.Learning.Dialogs
+{
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+    using Microsoft.Bot.Connector.Teams.Models;
+    using System;
+    using System.Threading.Tasks;
+
+    [Serializable]
+    public class GreetDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            // Create the connector client which will be used to make requests for roster and conversation data.
+            var connectorClient = new ConnectorClient(new Uri(context.Activity.ServiceUrl));
+
+            // Fetch members of this conversation and data from the current conversation.
+            var members = await connectorClient.Conversations.GetConversationMembersAsync(context.Activity.Conversation.Id);
+            var channelData = context.Activity.GetChannelData<TeamsChannelData>();
+            var bot = context.Activity.Recipient;
+
+            // For each member, create the conversation and send the greetings message
+            foreach (var member in members)
+            {
+                var parameters = new ConversationParameters
+                {
+                    Bot = bot,
+                    Members = new ChannelAccount[] { member },
+                    ChannelData = new TeamsChannelData
+                    {
+                        Tenant = channelData.Tenant
+                    }
+                };
+
+                // Create the conversation. If the bot has never talked to the user before this conversation will not exist; this ensures that the conversation exists.
+                var conversationResource = await connectorClient.Conversations.CreateConversationAsync(parameters);
+
+                // Create and send the response message.
+                var message = Activity.CreateMessageActivity();
+                message.From = bot;
+                message.Conversation = new ConversationAccount(id: conversationResource.Id);
+                message.Text = "Greetings!";
+                await connectorClient.Conversations.SendToConversationAsync((Activity)message);
+            }
+
+            context.Done<object>(null);
+        }
+    }
+}
